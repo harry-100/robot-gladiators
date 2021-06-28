aa072068313eb395d50ec2cff5757d97326b2840
Harry Shah <harvinder.shah@gmail.com>
2021-06-28 15:21:58 -0400

    variables defined

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! JavaScript is running")
+function fight() {
+    window.alert("The fight has begun");
+}
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+// fight();
+console.log(10 + 15);
+console.log ("Our robot's  name is " + playerName);
+var name = "Your Name";
+console.log(name);
