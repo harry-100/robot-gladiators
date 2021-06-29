257cb3bdfafbd8140d40e805ac447640ae57e1b9
Harry Shah <harvinder.shah@gmail.com>
2021-06-29 06:37:43 -0400

    first round completed

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,9 +2,42 @@ function fight() {
     window.alert("The fight has begun");
 }
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
-// fight();
-console.log(10 + 15);
-console.log ("Our robot's  name is " + playerName);
-var name = "Your Name";
-console.log(name);
+var playerHealth = 100;
+var playerAttack = 10;
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    // Alert players that they are starting the new round
+    window.alert("Welcome to Robot Gladiators!");
+    
+    /* Subtract the value of 'playerAttack' from the value of 'enemyHealth'
+    and use that result to update the value in 'enemyHealth' variable. */
+    enemyHealth = enemyHealth - playerAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.")
+    // check enemy's health
+    if (enemyHealth<=0) {
+        console.log(enemyName + " has died!");
+    }
+    else {
+        console.log(enemyName + " still has " + enemyHealth + " health left.")
+    }
+    /* Subtract the value of 'enemyAttack' from the value of 'playerHealth'
+    and use that result to update the value in the 'playerHealth' variable */
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked. 
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health  remaining.")
+
+    if (playerHealth<=0) {
+        console.log(playerName + " has died!")
+    }
+    else {
+        console.log(playerName + " attacked. " + playerName + " has " + playerHealth + " health remaining.")
+    }
+};
+
+fight();
+
+// Subtract the value of "playerAttack" from the value of "enemyHealth"
